7c6a05bb946c0c07e8458819b34fdc9016f0188a
Eyad <e.kam12@yahoo.com>
2024-06-12 00:40:58 -0400

    trying to escape iframe styles

--- site/config/GoogleMapsEmbed.tsx
+++ site/config/GoogleMapsEmbed.tsx
@@ -3,9 +3,9 @@ export default function GoogleMapsEmbed() {
     <div className="h-[80vh] w-full overflow-hidden rounded-lg border-2  border-primary/50 shadow-lg lg:h-[50vh]">
       <iframe
         src="https://storage.googleapis.com/maps-solutions-hkl2kro3e4/locator-plus/gprp/locator-plus.html"
-        width="100%"
+        width="100%;"
         style={{ border: 0 }}
-        height="100%"
+        height="100%;"
         loading="lazy"
         className="rounded-lg"
       ></iframe>
